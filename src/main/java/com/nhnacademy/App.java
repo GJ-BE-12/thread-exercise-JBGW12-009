116528afb5541bddccd2efdf210e22f51a9ee19b
Minseo Kim <porori031224@gmail.com>
2025-08-20 14:46:33 +0900

    exercise-step1-6-counter-join-2 실습 완료

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -20,20 +20,50 @@ public class App
 {
     public static void main( String[] args )
     {
-        CounterHandler counterHandler = new CounterHandler(10l);
-        Thread thread = new Thread(counterHandler);
-        log.debug("thread-state:{}",thread.getState());
-        thread.setName("my-counter");
-        thread.start();
-        //TODO#1 thread가 실행 후 (1-10 count 증가 후  아래 로그가 출력 됩니다.)
-        //thread.join()을 호출 하면 thread가 종료될 때 까지 main thread가 대기하게 됩니다.
+        //TODO#1 counterHandlerA 객체를 생성 합니다. countMaxSize : 10
+        CounterHandler counterHandlerA = new CounterHandler(10);
+        //TODO#2 threadA 생성시 counterHandlerA 객체를 paramter로 전달 합니다.
+        Thread threadA = new Thread(counterHandlerA);
+        //TODO#3 threadA의 name을 'my-counter-A' 로 설정 합니다.
+        threadA.setName("my-counter-A");
+
+        log.debug("threadA-state:{}",threadA.getState());
+
+
+        //TODO#4 counterHandlerB 객체를 생성 합니다. countMaxSize : 10
+        CounterHandler counterHandlerB = new CounterHandler(10);
+        //TODO#5 threadB 생성시 counterHandlerB 객체를 paramter로 전달 합니다.
+        Thread threadB = new Thread(counterHandlerB);
+        //TODO#6 threadB의 name을 'my-counter-B' 로 설정 합니다.
+        threadB.setName("my-counter-B");
+
+        log.debug("threadB-state:{}",threadB.getState());
+
+        //TODO#7 threadA를 시작 합니다.
+        threadA.start();
+
+        //TODO#8 threadA 작업이 완료될 때까지 main Thread는 대기 합니다.
         try {
-            thread.join();
+            threadA.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
+        log.debug("threadA-state:{}",threadA.getState());
+
+        //TODO#9 threadB를 시작 합니다.
+        threadB.start();
+
+        //TODO#10 threadB 작업이 완료될 때까지 main Thread는 대기 합니다.
+        try {
+            threadB.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        log.debug("threadB-state:{}",threadB.getState());
+
+        //TODO#11 'Application exit!' message를 출력 합니다.
         log.debug("Application exit!");
-        log.debug("thread-state:{}",thread.getState());
     }
 }
