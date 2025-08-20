0231e9c5eda4a3f93376680c1e6756895f4c42b1
Minseo Kim <porori031224@gmail.com>
2025-08-20 17:27:17 +0900

    step3-3-starvation 실습 완료

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -12,56 +12,69 @@
 
 package com.nhnacademy;
 
+import com.nhnacademy.count.SharedCounter;
+import com.nhnacademy.thread.CounterIncreaseHandler;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Map;
+
 @Slf4j
-public class App {
+public class App
+{
 
-    private static final Object resource1 = new Object();
-    private static final Object resource2 = new Object();
+    public static void main( String[] args )
+    {
 
-    public static void main(String[] args) {
+        //TODO#1 shardCounter 객체를 0으로 초기화 합니다.
+        SharedCounter sharedCounter = new SharedCounter(0l);
 
-        //TODO#1 Thread-1 가 resource2의 접근 권한을 획득한 상태에서 resource1의 접근 권한을 대기하고 있습니다.
-        Thread thread1 = new Thread(() -> {
-            synchronized (resource2) {
-                log.debug("{}: locked resource 2", Thread.currentThread().getName() );
+        //TODO#2 counterIncreaseHandler 객체를 생성 합니다.
+        CounterIncreaseHandler counterIncreaseHandler = new CounterIncreaseHandler(sharedCounter);
 
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    log.debug(e.getMessage());
-                }
+        //TODO#3 counterIncreaseHandler를 이용해서 threadA ~ E 를 생성 합니다.
+        Thread threadA = new Thread(counterIncreaseHandler, "thread-A");
+        Thread threadB = new Thread(counterIncreaseHandler, "thread-B");
+        Thread threadC = new Thread(counterIncreaseHandler, "thread-C");
+        Thread threadD = new Thread(counterIncreaseHandler, "thread-D");
+        Thread threadE = new Thread(counterIncreaseHandler, "thread-E");
 
-                synchronized (resource1) {
-                    log.debug("{}: locked resource 1", Thread.currentThread().getName());
-                }
-            }
+        //TODO#4 thrad의 우선순위를 지정할 수 있지만, 실질적인 관리는 운영체제에서 함으로, 동작하는 순서는 다를 수 있습니다.
+        threadA.setPriority(10);
+        threadB.setPriority(1);
+        threadC.setPriority(1);
+        threadD.setPriority(1);
+        threadE.setPriority(1);
+
+        //TODO#5 threadA ~ E 를 시작 합니다.
+        threadA.start();
+        threadB.start();
+        threadC.start();
+        threadD.start();
+        threadE.start();
+
+        //TODO#6 main thread가 실행 후 10초 후 threadA, threadB 종료될 수 있도록 interrupt 발생 시킵니다.
+        try {
+            Thread.sleep(10000);
+            threadA.interrupt();
+            threadB.interrupt();
+            threadC.interrupt();
+            threadD.interrupt();
+            threadE.interrupt();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        //TODO#7 main Thread는 threadA와 threadB의 상태가 terminated가 될 때 까지 대기 합니다. 즉 threadA, threadB가 종료될 때 까지 대기(양보) 합니다.
+        while (threadA.isAlive() && threadB.isAlive() && threadC.isAlive() && threadD.isAlive() && threadE.isAlive() ){
+            Thread.yield();
+        }
+
+        Map<String, Long> result = counterIncreaseHandler.getThreadHistory();
+        result.forEach((k,v)->{
+            log.debug("{}:{}",k,v);
         });
-        thread1.setName("Thread-1");
 
-        //TODO#2 Thread-2 가 resource1의 접근 권한을 획득한 상태에서 resource2의 접근 권한을 대기하고 있습니다.
-        Thread thread2 = new Thread(() -> {
-            synchronized (resource1) { // resource1을 먼저 잠금
-                log.debug("{}: locked resource 1",Thread.currentThread().getName());
+        log.debug("System exit!");
 
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    log.debug(e.getMessage());
-                }
-
-                synchronized (resource2) {
-                    log.debug("{}: locked resource 2",Thread.currentThread().getName());
-                }
-            }
-        });
-        thread2.setName("Thread-2");
-
-
-        thread1.start();
-        thread2.start();
     }
-
-
 }
