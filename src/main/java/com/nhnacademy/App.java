ebebfeaa7cc6acf01e66fe0fb4e63b0a22eda47e
Minseo Kim <porori031224@gmail.com>
2025-08-20 16:20:24 +0900

    exercise-step2-1-race-condition 실습 완료

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -12,55 +12,59 @@
 
 package com.nhnacademy;
 
-import com.nhnacademy.thread.CounterHandler;
+import com.nhnacademy.count.SharedCounter;
+import com.nhnacademy.thread.CounterIncreaseHandler;
 import lombok.extern.slf4j.Slf4j;
-
-import javax.management.monitor.CounterMonitor;
-import javax.management.monitor.Monitor;
 
 @Slf4j
 public class App
 {
 
-    //TODO#1 monitor로 사용한 객체를 생성 합니다.
-    public static Object monitor = new Object();
-
     public static void main( String[] args )
     {
 
-        //TODO#2 counterHandlerA 객체를 생성 합니다. countMaxSize : 10, monitor
-        CounterHandler counterHandlerA = new CounterHandler(10, monitor);
+        //TODO#1 shardCounter 객체를 0으로 초기화 합니다.
+        SharedCounter sharedCounter = new SharedCounter(0);
 
-        //threadA 생성시 counterHandlerA 객체를 paramter로 전달 합니다.
-        Thread threadA = new Thread(counterHandlerA);
+        //TODO#2 counterIncreaseHandler 객체를 생성 합니다.
+        CounterIncreaseHandler counterIncreaseHandler = new CounterIncreaseHandler(sharedCounter);
 
-        //threadA의 name을 'my-counter-A' 로 설정 합니다.
-        threadA.setName("my-counter-A");
-        log.debug("threadA-state:{}",threadA.getState());
+        //TODO#3 counterIncreaseHandler를 이용해서 threadA를 생성 합니다.
+        Thread threadA = new Thread(counterIncreaseHandler);
 
-        //threadA를 시작 합니다.
+        //TODO#4 threadA의 thread name을 "thread-A"로 설정 합니다.
+        threadA.setName("thread-A");
+
+        //TODO#5 threadA를 시작 합니다.
         threadA.start();
-        log.debug("threadA-state:{}",threadA.getState());
 
-        //TODO#3 - Main Thread에서 2초 후 monitor를 이용하여 대기하고 있는 threadA를 깨움 니다.
+        //TODO#6 counterIncreaseHandler를 이용해서 threadB를 생성 합니다.
+        Thread threadB = new Thread(counterIncreaseHandler);
+
+        //TODO#7 threadB의 name을 'thread-B' 로 설정 합니다.
+        threadB.setName("thread-B");
+
+        //TODO#8 threadB를 시작 합니다.
+        threadB.start();
+
+        //TODO#9 main thread가 실행 후 20초 후 threadA, threadB 종료될 수 있도록 interrupt 발생 시킵니다.
         try {
-            Thread.sleep(2000);
+            Thread.sleep(20000);
+
+            threadA.interrupt();
+            threadB.interrupt();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        synchronized (monitor) {
-            monitor.notify();
+        //TODO#10 main Thread는 threadA와 threadB의 상태가 terminated가 될 때 까지 대기 합니다. 즉 threadA, threadB가 종료될 때 까지 대기(양보) 합니다.
+        try {
+            threadA.join();
+            threadB.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
 
-        //Main Thread가 threadA  종료될 때 까지 대기 합니다. Thread.yield를 사용 합니다.
-        do {
-            Thread.yield();
-        }while (threadA.isAlive());
-
-        //'Application exit!' message를 출력 합니다.
-        log.debug("Application exit!");
-
+        log.debug("System exit!");
     }
-
 }
