c412b30a96c6d9366692c657efc4b9f214a6d7c0
Minseo Kim <porori031224@gmail.com>
2025-08-20 14:41:16 +0900

    exercise-step1-5-counter-join-1 실습 완료

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -13,30 +13,27 @@
 package com.nhnacademy;
 
 import com.nhnacademy.thread.CounterHandler;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class App
 {
     public static void main( String[] args )
     {
-        //TODO#1 CounterHandler 객체를 생성 합니다. countMaxSize : 10
-        CounterHandler counterHandler = new CounterHandler(10);
+        CounterHandler counterHandler = new CounterHandler(10l);
+        Thread thread = new Thread(counterHandler);
+        log.debug("thread-state:{}",thread.getState());
+        thread.setName("my-counter");
+        thread.start();
+        //TODO#1 thread가 실행 후 (1-10 count 증가 후  아래 로그가 출력 됩니다.)
+        //thread.join()을 호출 하면 thread가 종료될 때 까지 main thread가 대기하게 됩니다.
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
-        //TODO#2 threadA 생성시 counterHandler 객체를 paramter로 전달 합니다.
-        Thread threadA = new Thread(counterHandler);
-
-        //TODO#3 threadA의 name을 'my-counter-A' 로 설정 합니다.
-        threadA.setName("my-counter-A");
-
-        //TODO#4 threadB 생성시 counterHandler 객체를 paramter로 전달 합니다.
-        Thread threadB = new Thread(counterHandler);
-
-        //TODO#5 threadB의 name을 'my-counter-B' 로 설정 합니다.
-        threadB.setName("my-counter-B");
-
-        //TODO#7 threadA를 시작 합니다.
-        threadA.start();
-
-        //TODO#8 threadB를 시작 합니다.
-        threadB.start();
+        log.debug("Application exit!");
+        log.debug("thread-state:{}",thread.getState());
     }
 }
